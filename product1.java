489df0908e9872f50cfcfed6691c91417e63a7aa
pooja48-patil <poojabadappanavar89@gmail.com>
2025-04-04 11:04:33 +0530

    save

---
+++ product1.java
@@ -0,0 +1,28 @@
+
+class product {
+    String name;
+    int price;
+    strnt quantity;
+
+    
+    public product(String name, int price) {
+        this.name = name;
+        this.price = price;
+        this.quantity=quantity;
+    }
+
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("quantity: " + quantity);
+
+
+    }
+
+    
+    public static void main(String[] args) {
+        product p1 = new product("Car", 100000,"good");
+        p1.display();
+    }
+}
